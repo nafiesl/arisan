a5fd06bce5c53592a5b46f2f2e81a46b1e60a74e
Nafies Luthfi <nafiesl@gmail.com>
2018-04-16 23:00:07 +0800

    Prepare home layout, css and javascripts
    Update app font to use "Trebuchet MS" by default

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
+    .options({
+        processCssUrls: false
+    })
    .sass('resources/assets/sass/app.scss', 'public/css');
