0896f7fa40637db01aada75dbc3f279288ac18eb
Nafies Luthfi <nafiesl@gmail.com>
2018-04-27 07:34:32 +0800

    Update luthfi/simple-crud-generator package

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -1,15 +1,15 @@
 <?php
 
-use App\User;
 use App\Group;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Group::class, function (Faker $faker) {
 
     return [
-        'name' => $faker->word,
+        'name'        => $faker->words(2, true),
         'description' => $faker->sentence,
-        'creator_id' => function () {
+        'creator_id'  => function () {
             return factory(User::class)->create()->id;
         },
     ];
